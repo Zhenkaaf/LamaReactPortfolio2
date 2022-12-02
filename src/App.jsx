40160a8492f3b60f4cc0c1e576cedd37986ada42
Zhenkaaf <zhenkaaf@gmail.com>
2022-12-02 19:21:01 +0200

    useRefKeyframesiTyped

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
 import './app.scss';
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 
 
@@ -16,6 +17,7 @@ const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,40 @@
 import './intro.scss';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 
 const Intro = () => {
+
+    const textRef = useRef();
+    useEffect(() => {
+
+        init(textRef.current, {
+            showCursor: false,
+            backDelay:  2000,
+            showCursor: true,
+            strings: ['Developer', 'Designer']
+        })
+    }, [])
+
     return (
         <div className="intro" id='intro'>
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Fodorov Yevhen</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <KeyboardArrowDownIcon className='keyboardArrowDownIcon' />
+                    {/*  <img src="" alt="" /> */}
+                </a>
+            </div>
         </div>
     )
 }
