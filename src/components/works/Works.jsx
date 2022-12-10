2e58c552314c25757a1fe8dd6c314c2a9df964a1
Zhenkaaf <zhenkaaf@gmail.com>
2022-12-10 14:56:05 +0200

    slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,10 +1,71 @@
 import './works.scss';
-
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import DomainVerificationIcon from '@mui/icons-material/DomainVerification';
+import { useState } from 'react';
 
 const Works = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const data = [
+        {
+            id: '1',
+            icon: <PhoneAndroidIcon />,
+            title: 'Web Dedsign',
+            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis quaerat magni laudantium',
+            img: 'https://i.pinimg.com/564x/28/3f/58/283f5890c83f7d08c74fc7e886b5d71d.jpg'
+        },
+        {
+            id: '2',
+            icon: <AlternateEmailIcon />,
+            title: 'Web Dedsign',
+            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis quaerat magni laudantium',
+            img: 'https://images.pexels.com/photos/1742370/pexels-photo-1742370.jpeg?auto=compress&cs=tinysrgb&w=600'
+        },
+        {
+            id: '3',
+            icon: <DomainVerificationIcon />,
+            title: 'Web Dedsign',
+            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis quaerat magni laudantium',
+            img: 'https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=600'
+        }
+
+    ];
+
+    const handleClick = (way) => {
+        way === 'left' ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) 
+        : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    } 
+
     return (
         <div className="works" id='works'>
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
 
+                {data.map(item => (
+                    <div className="container" key={item.id}>
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    {item.icon}
+                                    {/* <img src="" alt="" /> */}
+                                </div>
+                                <h2>{item.title}</h2>
+                                <p>{item.desc}</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src={item.img} alt="" />
+                        </div>
+                    </div>
+                </div>
+                ))}
+
+            </div>
+            <ArrowBackIosIcon className="arrow left" onClick={() => handleClick('left')} />
+            <ArrowForwardIosIcon className="arrow right" onClick={() => handleClick()} />
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,164 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: 'Social Media App',
+        img: 'https://images.pexels.com/photos/6633920/pexels-photo-6633920.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 2,
+        title: 'Rampa UI Design',
+        img: 'https://images.pexels.com/photos/7651559/pexels-photo-7651559.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'E-commerce Web Design',
+        img: 'https://images.pexels.com/photos/10474224/pexels-photo-10474224.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 4,
+        title: 'Web Relax App',
+        img: 'https://images.pexels.com/photos/10474131/pexels-photo-10474131.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 5,
+        title: 'Simple Media App',
+        img: 'https://images.pexels.com/photos/5690488/pexels-photo-5690488.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 6,
+        title: 'Social Media Web Design',
+        img: 'https://images.pexels.com/photos/10474158/pexels-photo-10474158.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: 'Social Media App',
+        img: 'https://images.pexels.com/photos/4126724/pexels-photo-4126724.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 2,
+        title: 'Rampa UI Design',
+        img: 'https://images.pexels.com/photos/3803517/pexels-photo-3803517.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'E-commerce Web Design',
+        img: 'https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 4,
+        title: 'Web Relax App',
+        img: 'https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 5,
+        title: 'Simple Media App',
+        img: 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 6,
+        title: 'Social Media Web Design',
+        img: 'https://images.pexels.com/photos/4050290/pexels-photo-4050290.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: 'Social Media App',
+        img: 'https://images.pexels.com/photos/147413/twitter-facebook-together-exchange-of-information-147413.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 2,
+        title: 'Rampa UI Design',
+        img: 'https://images.pexels.com/photos/38544/imac-apple-mockup-app-38544.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'E-commerce Web Design',
+        img: 'https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 4,
+        title: 'Web Relax App',
+        img: 'https://images.pexels.com/photos/33488/navigation-car-drive-road.jpg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 5,
+        title: 'Simple Media App',
+        img: 'https://images.pexels.com/photos/3776659/pexels-photo-3776659.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 6,
+        title: 'Social Media Web Design',
+        img: 'https://images.pexels.com/photos/6177645/pexels-photo-6177645.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: 'Social Media App',
+        img: 'https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 2,
+        title: 'Rampa UI Design',
+        img: 'https://images.pexels.com/photos/1762851/pexels-photo-1762851.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'E-commerce Web Design',
+        img: 'https://images.pexels.com/photos/355948/pexels-photo-355948.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 4,
+        title: 'Web Relax App',
+        img: 'https://images.pexels.com/photos/1212407/pexels-photo-1212407.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 5,
+        title: 'Simple Media App',
+        img: 'https://images.pexels.com/photos/2097/desk-office-pen-ruler.jpg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 6,
+        title: 'Social Media Web Design',
+        img: 'https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
+
+export const contentPortfolio = [
+    {
+        id: 1,
+        title: 'Social Media App',
+        img: 'https://images.pexels.com/photos/9687693/pexels-photo-9687693.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 2,
+        title: 'Rampa UI Design',
+        img: 'https://images.pexels.com/photos/1766604/pexels-photo-1766604.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 3,
+        title: 'E-commerce Web Design',
+        img: 'https://images.pexels.com/photos/4348196/pexels-photo-4348196.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 4,
+        title: 'Web Relax App',
+        img: 'https://images.pexels.com/photos/4474031/pexels-photo-4474031.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 5,
+        title: 'Simple Media App',
+        img: 'https://images.pexels.com/photos/6954192/pexels-photo-6954192.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        id: 6,
+        title: 'Social Media Web Design',
+        img: 'https://images.pexels.com/photos/3183170/pexels-photo-3183170.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
